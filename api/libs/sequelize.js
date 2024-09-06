7b6dd66e14424524c663fd6877f5d86d5acdf2da
econdorir <econdorir@fcpn.edu.bo>
2024-09-06 18:43:32 +0000

    setting sequelize config

---
+++ api/libs/sequelize.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require("sequelize");
+const { config } = require("../config/config");
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+const sequelize = new Sequelize(URI, {
+  dialect: "mysql",
+  logging: true,
+});
+
+setUpModels(sequelize);
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Base de datos y tablas sincronizadas");
+  })
+  .catch((error) => {
+    console.error("Error al sincronizar base de datos", error);
+  });
+module.exports = sequelize;
