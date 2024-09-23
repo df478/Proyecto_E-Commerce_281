c2e01ec80795f8ee597d565cf1c1842f07748495
Ahet34 <alantintaya34@gmail.com>
2024-09-22 23:30:13 -0400

    arreglando routers

--- api/routes/aniade.router.js
+++ api/routes/aniade.router.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const ProPromCliCarService = require('./../services/pro_prom_cli_car.service');
+const AniadeService = require('./../services/aniade.service');
 const validatorHandler = require("../middlewares/validator.handler");
 const router = express.Router();
-const service = new ProPromCliCarService();
+const service = new AniadeService();
 const {
-    obtenerProPromCliCarSchema,
-    crearProPromCliCarSchema,
-    actualizarProPromCliCarSchema,
-} = require("../schemas/pro_prom_cli_car.schema");
+    obtenerAniadeSchema,
+    crearAniadeSchema,
+    actualizarAniadeSchema,
+} = require("../schemas/aniade.schema");
 
 router.get('/', async (req, res) => {
     try {
@@ -20,24 +20,24 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id_producto/:id_carrito/:id_cliente', 
-    validatorHandler(obtenerProPromCliCarSchema, "params"),
+    validatorHandler(obtenerAniadeSchema, "params"),
     async (req, res) => {
         try {
             const { id_producto, id_carrito, id_cliente } = req.params;
             const aniadido = await service.findOne(id_producto, id_carrito, id_cliente);
             if (!aniadido) {
-                return res.status(404).json({ message: 'Añadido no encontrado' });
+                return res.status(404).json({ message: 'Aniade no encontrado' });
             }
             res.json(aniadido);
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: 'Error al obtener el añadido', error: error.message });
+            res.status(500).json({ message: 'Error al obtener la aniade', error: error.message });
         }
     }
 );
 
 router.post('/', 
-    validatorHandler(crearProPromCliCarSchema, "body"),
+    validatorHandler(crearAniadeSchema, "body"),
     async (req, res) => {
         try {
             const body = req.body;
@@ -45,43 +45,43 @@ router.post('/',
             res.status(201).json(nuevoAniadido);
         } catch (error) {
             console.error(error);
-            res.status(400).json({ message: 'Error al crear el añadido', error: error.message });
+            res.status(400).json({ message: 'Error al crear la aniade', error: error.message });
         }
     }
 );
 
 router.patch('/:id_producto/:id_carrito/:id_cliente', 
-    validatorHandler(obtenerProPromCliCarSchema, "params"),
-    validatorHandler(actualizarProPromCliCarSchema, "body"),
+    validatorHandler(obtenerAniadeSchema, "params"),
+    validatorHandler(actualizarAniadeSchema, "body"),
     async (req, res) => {
         try {
             const { id_producto, id_carrito, id_cliente } = req.params;
             const body = req.body;
             const aniadido = await service.update(id_producto, id_carrito, id_cliente, body);
             if (!aniadido) {
-                return res.status(404).json({ message: 'Añadido no encontrado' });
+                return res.status(404).json({ message: 'Aniade no encontrada' });
             }
             res.json(aniadido);
         } catch (error) {
             console.error(error);
-            res.status(400).json({ message: 'Error al actualizar el añadido', error: error.message });
+            res.status(400).json({ message: 'Error al actualizar la aniade', error: error.message });
         }
     }
 );
 
 router.delete('/:id_producto/:id_carrito/:id_cliente', 
-    validatorHandler(obtenerProPromCliCarSchema, "params"),
+    validatorHandler(obtenerAniadeSchema, "params"),
     async (req, res) => {
         try {
             const { id_producto, id_carrito, id_cliente } = req.params;
             const rta = await service.delete(id_producto, id_carrito, id_cliente);
             if (!rta) {
-                return res.status(404).json({ message: 'Añadido no encontrado' });
+                return res.status(404).json({ message: 'Aniade no encontrada' });
             }
             res.json(rta);
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: 'Error al eliminar el añadido', error: error.message });
+            res.status(500).json({ message: 'Error al eliminar la aniade', error: error.message });
         }
     }
 );
