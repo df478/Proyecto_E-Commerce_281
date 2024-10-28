ac33f1e3233438ec23885216769bd55a30913577
eleazar <eledavid88@gmail.com>
2024-10-28 13:03:35 -0400

    atributo calificacion_resenia en la tabla resenia

--- api/schemas/resenia.schema.js
+++ api/schemas/resenia.schema.js
@@ -5,20 +5,22 @@ const id_producto = Joi.number().integer();
 const id_usuario = Joi.number().integer();
 const descripcion_resenia = Joi.string().min(4).max(100);
 const fecha_resenia = Joi.date();
+const calificacion_resenia = Joi.number().min(1).max(5).precision(1);
 
 const crearReseniaSchema = Joi.object({
     id_producto: id_producto.required(),
     id_usuario: id_usuario.required(),
     descripcion_resenia: descripcion_resenia.required(),
-    fecha_resenia: fecha_resenia.required()
+    fecha_resenia: fecha_resenia.required(),
+    calificacion_resenia: calificacion_resenia.required()
 });
-
 
 const actualizarReseniaSchema = Joi.object({
     id_producto: id_producto,
     id_usuario: id_usuario,
     descripcion_resenia: descripcion_resenia,
-    fecha_resenia: fecha_resenia
+    fecha_resenia: fecha_resenia,
+    calificacion_resenia: calificacion_resenia
 });
 
 const obtenerReseniaSchema = Joi.object({
@@ -29,4 +31,4 @@ module.exports = {
     crearReseniaSchema,
     actualizarReseniaSchema,
     obtenerReseniaSchema
-};   
+};
