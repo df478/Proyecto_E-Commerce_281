6f4e4a05bf9411d790952671426263bb5e8a27d8
eleazar <eledavid88@gmail.com>
2024-11-12 10:30:38 -0400

    despliegue

--- apis/index.js
+++ apis/index.js
@@ -1,88 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const routerApi = require("./routes");
-const { logErrors, boomErrorHandler, errorHandler, ormErrorHandler } = require("./middlewares/error.handler");
-// const sequelize = require('./libs/sequelize');
+const routerApi = require("./routes");  // Aquí importamos tu routerApi
 
 const app = express();
-const port = process.env.PORT || 3000; //puerto del servidor
 
-app.use(express.json());
-
-const whitelist = [
-  "http://localhost:5500",
-  "https://myapp.co",
-  "http://localhost:3000",
-  "http://localhost:5050",
-  "http://localhost:5000",
-];
-// const options = {
-//   origin: (origin, callback) => {
-//     if (whitelist.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("no permitido"));
-//     }
-//   },
-// };
-
+// Configuración de CORS
 const corsOptions = {
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
+  origin: 'http://localhost:3000',
+  credentials: true, // Permitir cookies y credenciales
+  optionSuccessStatus: 200
+};
+
 app.use(cors(corsOptions));
-require('./utils/auth')
+app.use(express.json());  // Middleware para analizar JSON
 
-app.get("/api", (req, res) => {
-  res.send("Hola mi server en express");
-});
+// Rutas de la API
+routerApi(app);  // Usamos el routerApi que gestiona todas las rutas
 
-app.get("/api/nueva-ruta", (req, res) => {
-  res.send("Hola soy un nuevo endpoint");
-});
-
-app.get("/home", (req, res) => {
-  res.send("Home :)");
-});
-
-app.get("/archivo-json", (req, res) => {
-  res.json([
-    {
-      nombre: "Alan",
-      edad: 21,
-    },
-    {
-      nombre: "Juan",
-      edad: 54,
-    },
-    {
-      nombre: "Ron",
-      edad: 15,
-    },
-  ]);
-});
-
-app.get("/queriesxd", (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    })
-  } else {
-    res.send("No hay parametros D:")
-  }
-});
-
-
-routerApi(app);
-// app.use(cors(options));
-app.use(logErrors);
-app.use(boomErrorHandler);
-app.use(ormErrorHandler);
-app.use(errorHandler);
-
-app.listen(port, () => {
-  console.log("Mi port " + port);
-});
+// Exportar la aplicación para que Vercel la maneje como función serverless
+module.exports = app;
