3c5714410a197ce7197626974d3a8353a43f1383
df478 <eledavid88@gmail.com>
2024-12-21 22:17:17 -0400

    mysql

--- api/db/config.js
+++ api/db/config.js
@@ -2,21 +2,15 @@ const { config } = require('../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgresql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
     development: {
         url: URI,
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-              require: true, // This forces SSL connection
-              rejectUnauthorized: false, // Optional: allows self-signed certificates
-            },
-          },
+        dialect: 'mysql',
     },
     production: {
         uri: URI,
-        dialect: 'postgres'
+        dialect: 'mysql'
     }
 }
